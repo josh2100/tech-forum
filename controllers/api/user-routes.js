285594667e75ce5a8b3c507427e8173ff751b543
joshgit <jschermann91@gmail.com>
2022-01-16 16:52:17 -0600

    user put route changed to asynchronous

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Post, Comment, Vote } = require("../../models");
+const { User, Post, Comment } = require("../../models");
 
 // Read all users api/users
 router.get("/", async (req, res) => {
@@ -31,7 +31,7 @@ router.get("/:id", async (req, res) => {
 
     res.status(200).json(singleUser);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
   }
 });
 
@@ -52,7 +52,7 @@ router.post("/", async (req, res) => {
 
     res.status(200).json(newUser);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
   }
 });
 
@@ -85,14 +85,13 @@ router.post("/login", async (req, res) => {
 
       res.json({ user: existingUser, message: "You are now logged in!" });
     });
-
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
   }
 });
 
-// Logout user
-router.post("/logout", async (req, res) => {
+// Logout user api/users/:id
+router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -102,29 +101,29 @@ router.post("/logout", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-  // pass in req.body instead to only update what's passed through
-  User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+// Update user api/users/:id
+router.put("/:id", async (req, res) => {
+  // expects { "username": "tiger", "email": "tiger@email.com", "password": "tiger"}
+  // pass in req.body to only update what's passed through
+  try {
+    const updatedUser = await User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id,
+      },
     });
+
+    if (!updatedUser) {
+      res.status(404).json({ message: "No user found with this id" });
+      return;
+    }
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
+//
 router.delete("/:id", (req, res) => {
   User.destroy({
     where: {
