5948053f277c5c06013f762e807f8233210812f6
joshgit <jschermann91@gmail.com>
2022-01-16 18:56:13 -0600

    get one post route refactored for async

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { Post, User, Vote, Comment } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 // Get all posts (api/posts/)
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
   try {
     const allPosts = await Post.findAll({
       // attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-         // id | title | user_id | post_text | date_created auto?
+      // id | title | user_id | post_text | date_created auto?
       // include: [
       //   {
       //     model: Post,
@@ -19,63 +19,30 @@ router.get("/", async (req, res) => {
       // ],
     });
 
-    // What does this line do?
-    // const posts = allPosts.map((post) => post.get({ plain: true }));
-
-    // res.render("homepage", {
-    //   posts,
-    //   loggedIn: req.session.loggedIn,
-    // });
     res.status(200).json(allPosts);
   } catch (error) {
     res.status(500).json(error);
   }
-});
+}); 
 
 // Read a single post by id api/categories/:id
-router.get("/:id", (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
+router.get("/:id", async (req, res) => {
+  try {
+    const onePost = await Post.findOne({
+      where: {
+        id: req.params.id,
       },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
-      res.json(dbPostData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
     });
+
+    if (!onePost) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+    res.json(onePost);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Create new post api/posts/
@@ -91,7 +58,7 @@ router.post("/", async (req, res) => {
       // user_id: req.session.user_id,
     });
 
-    res.status(200).json(`Post added: ${newPost.title}`)
+    res.status(200).json(`Post added: ${newPost.title}`);
   } catch (error) {
     res.status(500).json(error);
   }
