2fc0c0ed14f1225353954b3c32d47c654dad7248
joshgit <jschermann91@gmail.com>
2022-01-16 19:20:39 -0600

    delete route refactored for async

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -121,23 +121,25 @@ router.put("/:id", async (req, res) => {
 });
 
 // Delete a post (api/posts/:id)
-router.delete("/:id", withAuth, (req, res) => {
-  Post.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
-      res.json(dbPostData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+// router.delete("/:id", withAuth, (req, res) => {
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
     });
+
+    if (!deletedPost) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+
+    res.status(200).json(`Post ${req.params.id} has been deleted`);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
